87baad60a8e78e3feedcd7262dc6801e85a45512
QSL <kbeechly@icloud.com>
2022-08-21 00:41:14 -0700

    Refactor AI pseudocode

--- ticTacToebot.js
+++ ticTacToebot.js
@@ -1,9 +1,9 @@
 
 
 // Grid:
-//  TL TC TR
-//  ML MC MR
-//  BL BC BR
+//  TL TC TR   X    Y   C S C
+//  ML MC MR     XY     S I S
+//  BL BC BR   Y    X   C S C
 
 // UNBEATABLE AI:  
 // if (any row or column contains two AI marks and zero human marks) {
@@ -23,16 +23,16 @@
 // }
 //
 //
-// if (there exists a corner that shares a row or column with one other AI mark and zero human marks) {
-//     mark that corner
+// if (any row or column contains one AI marked corner and zero human marks) {
+//     mark the other corner in that row or column
 // }
 //
 // if (human has 2 or more corners marked and AI has 0 corners marked) {
 //     mark a random side
-// }
+// } [redundant for Unbeatable Master]
 //
 // if (human has 1 corner marked and AI has 0 marks on the board) {
-//     mark the center
+//     mark the inner square
 // }
 //
 // if (any corners are available) {
@@ -41,8 +41,20 @@
 //
 // if (any sides are available) {
 //     mark a random side
-// }
+// } [redundant for Unbeatable Master]
 //
 // else {
 //     mark a random square
 // }
+
+// Use counters to tally up total numbers of marks in groups. For example,
+// the inner square would +1 to the middle, center, X, Y, and inner counters.
+// The center left squre would +1 to the center, left, and side counters.
+
+// Give AI a 'memory bank' of rows, columns, and diagonals (winSets) that can still be
+// used for a winning play. IE it would start out as [T, M, B, L, C, R, X, Y].
+// As human player marks squares, remove those winSets
+// from the AI 'memory bank.'
+
+// AI SKILL LEVELS: Random Picker, Distracted Doodler, Formidible Challenger, Unbeatable Master
+// AI RANDOM PICK PERCENTAGE: 100%; 50%; 10%; 0%

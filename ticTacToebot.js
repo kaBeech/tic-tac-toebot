65e5964552b3922fe1abb4f7fba3fb712be7ae1e
QSL <kbeechly@icloud.com>
2022-08-20 23:27:35 -0700

    Add AI pseudocode

---
+++ ticTacToebot.js
@@ -0,0 +1,40 @@
+
+
+// Grid:
+//  TL TC TR
+//  ML MC MR
+//  BL BC BR
+
+// AI:  
+// if (any row or column contains two AI marks and zero human marks) {
+//     mark the third cell in that row or column
+// }
+//
+// if (any diagonal contains two AI marks and zero human marks) {
+//     mark the third cell in that diagonal
+// }
+//
+// if (any row or column contains two human marks and zero AI marks) {
+//     mark the third cell in that row or column
+// }
+//
+// if (any diagonal contains two human marks and zero AI marks) {
+//     mark the third cell in that diagonal
+// }
+//
+//
+// if (there exists a corner that shares a row or column with one other AI mark and zero human marks) {
+//     mark that corner
+// }
+//
+// if (human has 2 or more corners marked and AI has 0 corners marked) {
+//     mark a random side
+// }
+//
+// if (human has 1 corner marked and AI has 0 marks on the board) {
+//     mark the center
+// }
+//
+// if (any corners are available) {
+//     mark a random corner
+// }

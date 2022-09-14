7c73caaa9d2de6288247d2557936fd44e40878ad
QSL <kbeechly@icloud.com>
2022-09-14 12:58:34 -0700

    Implement viewsetter for smaller screens

--- viewSetter.js
+++ viewSetter.js
@@ -3,7 +3,7 @@ const player1CardDOM = document.querySelector('#player1Card');
 const player2CardDOM = document.querySelector('#player2Card');
 
 
-if (window.outerWidth < window.outerHeight) {
+if (window.outerWidth < 1000) {
     player1CardDOM.style.display = "none";
     player2CardDOM.style.display = "none";
 

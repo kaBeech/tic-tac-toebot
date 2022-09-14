59f63d8b2107a368b56994340a95c37853c231c3
QSL <kbeechly@icloud.com>
2022-09-13 17:12:48 -0700

    Add viewsetter

---
+++ viewSetter.js
@@ -0,0 +1,10 @@
+const player1CardDOM = document.querySelector('#player1Card');
+
+const player2CardDOM = document.querySelector('#player2Card');
+
+
+if (window.outerWidth < window.outerHeight) {
+    player1CardDOM.style.display = "none";
+    player2CardDOM.style.display = "none";
+
+}
